d9b231f6df28bfab2d4ff2558a25f042c0748868
Cathallane <Lanecathal@gmail.com>
2023-04-22 11:32:00 +0000

    commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "mbed.h"
+#include "C12832.h"
+InterruptIn joystickcenter(p14);
+InterruptIn button(p9);
+DigitalOut led(LED1);
+DigitalOut flash(LED4);
+C12832 lcd(p5, p7, p6, p8, p11);
+int x=0;
+void flip() {
+    x=1;
+    led = !led; // toggles the led when the joystick button is pressed.
+}
+int main() {
+    joystickcenter.rise(&flip); // attach the function address to the rising edge
+    button.mode(PullUp); // With this, no external pullup resistor needed
+    button.rise(&flip); // attach the function address to the rising edge
+    while(1) { // wait around, interrupts will interrupt this!
+        flash = !flash; // turns LED4 on if off, off if on
+        wait(0.25); // the instruction to wait for a quarter-second
+        if(x==1){
+            lcd.printf( "hello");
+            x=0;
+        }
+    }
+}
+
